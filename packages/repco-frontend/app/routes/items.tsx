ab745eba402bbfa72245891418f24ed1e5c6c5a1
nemi-notrace <nemi1012@h-ka.de>
2022-10-24 23:24:26 +0200

    playing around

--- packages/repco-frontend/app/routes/items.tsx
+++ packages/repco-frontend/app/routes/items.tsx
@@ -1,4 +1,4 @@
-import type { LoaderFunction } from '@remix-run/node'
+import { LoaderFunction, redirect } from '@remix-run/node'
 import { Form, NavLink, useFetcher, useLoaderData } from '@remix-run/react'
 import { gql } from '@urql/core'
 import { useEffect, useState } from 'react'
@@ -51,8 +51,8 @@ export const loader: LoaderFunction = ({ request }) => {
   const orderBy = url.searchParams.get('orderBy') || 'TITLE_ASC'
   const includes = url.searchParams.get('includes') || ''
   if (after && before) throw new Error('Invalid query arguments.')
-  const last = before ? 50 : null
-  const first = last ? null : 50
+  const last = before ? 10 : null
+  const first = last ? null : 10
 
   return graphqlQuery<LoadContentItemsQuery, LoadContentItemsQueryVariables>(
     QUERY,
@@ -69,6 +69,7 @@ export const loader: LoaderFunction = ({ request }) => {
 
 export default function IndexRoute() {
   const { data } = useLoaderData<LoaderData>()
+  console.log(data)
 
   const [pageInfo, setPageInfo] = useState(data.contentItems?.pageInfo)
   const [nodes, setNodes] = useState(data.contentItems?.nodes)
@@ -85,76 +86,81 @@ export default function IndexRoute() {
 
   useEffect(() => {
     if (shouldFetch) {
-      if (after) {
-        fetcher.load(
-          `/items?after=${pageInfo?.endCursor}&orderBy=${orderBy}&includes=${includes}`,
-        )
-        setShouldFetch(false)
-        setAfter(false)
-        return
-      }
+      //     if (after) {
+      //       fetcher.load(
+      //         `/items?after=${pageInfo?.endCursor}&orderBy=${orderBy}&includes=${includes}`,
+      //       )
+      //       setShouldFetch(false)
+      //       setAfter(false)
+      //       return
+      //     }
 
-      if (before) {
-        fetcher.load(
-          `/items?before=${pageInfo?.startCursor}&orderBy=${orderBy}&includes=${includes}`,
-        )
-        setShouldFetch(false)
-        setBefore(false)
-        return
-      }
+      //     if (before) {
+      //       fetcher.load(
+      //         `/items?before=${pageInfo?.startCursor}&orderBy=${orderBy}&includes=${includes}`,
+      //       )
+      //       setShouldFetch(false)
+      //       setBefore(false)
+      //       return
+      //     }
 
-      fetcher.load(`/items?orderBy=${orderBy}&includes=${includes}`)
+      //    fetcher.load(`/items?orderBy=${orderBy}&includes=${includes}`)
 
       setShouldFetch(false)
-      return
+    }
+    ;() => {
+      return redirect('/kjh')
     }
   }, [after, before, fetcher, includes, orderBy, pageInfo, shouldFetch])
 
-  useEffect(() => {
-    if (fetcher.data) {
-      console.log('FETCH', fetcher.data.data.contentItems.nodes)
-      setNodes(fetcher.data.data.contentItems.nodes)
-      setPageInfo(fetcher.data.data.contentItems.pageInfo)
-      console.log(pageInfo)
-    }
-  }, [fetcher.data, pageInfo])
+  // useEffect(() => {
+  //   if (fetcher.data) {
+  //     console.log('FETCH', fetcher.data.data.contentItems.nodes)
+  //     setNodes(fetcher.data.data.contentItems.nodes)
+  //     setPageInfo(fetcher.data.data.contentItems.pageInfo)
+  //     console.log(pageInfo)
+  //   }
+  // }, [fetcher.data, pageInfo])
 
   return (
     <main>
-      <div className="relative">
-        <div className="inlineSvg">
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
+      <Form action="/items" method="get">
+        <div className="relative">
+          <div className="inlineSvg">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            name="includes"
+            className="input"
+            placeholder="Search Titles..."
+            onChange={(e) => setSearchField(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="inlineBtn"
+            onClick={() => {
+              setIncludes(searchField)
+            }}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
+            Search
+          </button>
         </div>
-        <input
-          type="search"
-          id="default-search"
-          className="input"
-          placeholder="Search Titles..."
-          onChange={(e) => setSearchField(e.target.value)}
-        />
-        <button
-          className="inlineBtn"
-          onClick={() => {
-            setShouldFetch(true)
-            setIncludes(searchField)
-          }}
-        >
-          Search
-        </button>
-      </div>
+      </Form>
       <div>
         <label className="sr-only">Order By</label>
         <select
@@ -170,8 +176,9 @@ export default function IndexRoute() {
           <option value="TITLE_DESC">Title DESC</option>
         </select>{' '}
       </div>
-      {nodes &&
-        nodes.map((node, i) => (
+
+      {data.contentItems?.nodes &&
+        data.contentItems?.nodes.map((node, i) => (
           <div key={node.uid} className="card">
             <h5 className="h5">
               <SanitizedHTML allowedTags={['a', 'p']} html={node.title} />
@@ -210,57 +217,73 @@ export default function IndexRoute() {
         ))}
       <div>
         <div className="py-4 flex justify-center flex-row mx-auto">
-          {pageInfo?.hasPreviousPage && (
-            <button
-              type="button"
-              className="button"
-              onClick={() => {
-                setShouldFetch(true)
-                setBefore(true)
-              }}
+          {data.contentItems?.pageInfo?.hasPreviousPage && (
+            <NavLink
+              data-bs-toggle="tooltip"
+              title="next"
+              data-bs-placement="top"
+              prefetch="render"
+              to={`/items?before=${data.contentItems?.pageInfo?.startCursor}&orderBy=${orderBy}&includes=${includes}`}
             >
-              <div className="flex flex-row align-middle">
-                <svg
-                  className="w-5 mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <p className="ml-2">Prev</p>
-              </div>
-            </button>
+              <button
+                type="button"
+                className="button"
+                // onClick={() => {
+                //   setShouldFetch(true)
+                //   setBefore(true)
+                // }}
+              >
+                <div className="flex flex-row align-middle">
+                  <svg
+                    className="w-5 mr-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <p className="ml-2">Prev </p>
+                </div>
+              </button>
+            </NavLink>
           )}
-          {pageInfo?.hasNextPage && (
-            <button
-              type="button"
-              className="button"
-              onClick={() => {
-                setShouldFetch(true)
-                setAfter(true)
-              }}
+          {data.contentItems?.pageInfo?.hasNextPage && (
+            <NavLink
+              data-bs-toggle="tooltip"
+              title="next"
+              data-bs-placement="top"
+              prefetch="render"
+              to={`/items?after=${data.contentItems?.pageInfo?.endCursor}&orderBy=${orderBy}&includes=${includes}`}
             >
-              <div className="flex flex-row align-middle">
-                <span className="mr-2">Next</span>
-                <svg
-                  className="w-5 ml-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-              </div>
-            </button>
+              <button
+                type="button"
+                className="button"
+                // onClick={() => {
+                //   setShouldFetch(true)
+                //   setAfter(true)
+                // }}
+              >
+                <div className="flex flex-row align-middle">
+                  <span className="mr-2"> Next </span>
+                  <svg
+                    className="w-5 ml-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+            </NavLink>
           )}
         </div>
       </div>
